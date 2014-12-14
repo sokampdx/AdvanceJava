c8249df4b1f11a64c1c38cc9c9c91bbfe63f4613
sokampdx <sokam@pdx.edu>
2014-12-14 02:03:42 -0800

    refactor and complete num args test and valid option check

--- airline/src/main/java/edu/pdx/cs410J/sokam/Project1.java
+++ airline/src/main/java/edu/pdx/cs410J/sokam/Project1.java
@@ -7,44 +7,68 @@ import edu.pdx.cs410J.AbstractAirline;
  */
 public class Project1 {
 
-    public static final String USAGE = "usage: java edu.pdx.cs410J.sokam.Project1 [-(print|README)] name flightNumber src departTime dest arriveTime";
-    public static final String NOT_ENOUGH_COMMAND_LINE_ARGUMENTS = "Not enough command line arguments";
-    public static final String TOO_MANY_COMMAND_LINE_ARGUMENTS = "There are too many command line arguments.";
-    public static final int ARGUMENT_WITH_NO_OPTION = 6;
-    public static final int ARGUMENT_WITH_ONE_OPTION = 7;
-    public static final int ARGUMENT_WITH_TWO_OPTION = 8;
-    public static final String PRINT = "-print";
-    public static final String README = "README";
-    public static final String INCORRECT_COMMAND_LINE_ARGUMENTS = "Incorrect Command Line Arguments";
+  public static final String USAGE = "usage: java edu.pdx.cs410J.sokam.Project1 [-(print|README_OPTION)] name flightNumber src departTime dest arriveTime";
+  public static final String NOT_ENOUGH_COMMAND_LINE_ARGUMENTS = "Not enough command line arguments";
+  public static final String TOO_MANY_COMMAND_LINE_ARGUMENTS = "There are too many command line arguments.";
+  public static final int ARGUMENT_WITH_NO_OPTION = 8;
+  public static final int ARGUMENT_WITH_ONE_OPTION = 9;
+  public static final int ARGUMENT_WITH_TWO_OPTION = 10;
+  public static final String PRINT_OPTION = "-print";
+  public static final String README_OPTION = "-README";
+  public static final String INCORRECT_COMMAND_LINE_ARGUMENTS = "Incorrect Command Line Arguments";
 
-    public static void main(String[] args) {
-        Class c = AbstractAirline.class;  // Refer to one of Dave's classes so that we can be sure it is on the classpath
-        if (args.length < ARGUMENT_WITH_NO_OPTION)
-            printArgumentIsNotValid(NOT_ENOUGH_COMMAND_LINE_ARGUMENTS);
-        if (args.length > ARGUMENT_WITH_TWO_OPTION)
-            printArgumentIsNotValid(TOO_MANY_COMMAND_LINE_ARGUMENTS);
-        if (args.length == (ARGUMENT_WITH_ONE_OPTION) && isNotOption(args[0]))
-            printArgumentIsNotValid(INCORRECT_COMMAND_LINE_ARGUMENTS);
-        if (args.length == (ARGUMENT_WITH_TWO_OPTION) && (args[0] == args[1] || isNotOption(args[0]) || isNotOption(args[1])))
-            printArgumentIsNotValid(INCORRECT_COMMAND_LINE_ARGUMENTS);
+  public static void main(String[] args) {
+    Class c = AbstractAirline.class;  // Refer to one of Dave's classes so that we can be sure it is on the classpath
 
-        System.exit(0);
+    if (isWithinArgsRange(args) && isValidOption(args))
+      System.exit(0);
+    else
+      System.exit(1);
 //        for (String arg : args) {
 //          System.out.println(arg);
 //        }
+  }
+
+  private static boolean isValidOption(String[] args) {
+    boolean isValid = true;
+
+    if (args.length == (ARGUMENT_WITH_ONE_OPTION) && isNotOption(args[0])) {
+      printArgumentIsNotValid(INCORRECT_COMMAND_LINE_ARGUMENTS);
+      isValid = false;
+    }
+    if (args.length == (ARGUMENT_WITH_TWO_OPTION) &&
+        (args[0] == args[1] || isNotOption(args[0]) || isNotOption(args[1]))) {
+      printArgumentIsNotValid(INCORRECT_COMMAND_LINE_ARGUMENTS);
+      isValid = false;
     }
 
-    private static boolean isNotOption(String args) {
-        return !(args == PRINT || args == README);
+    return isValid;
+  }
+
+  private static boolean isWithinArgsRange(String[] args) {
+    boolean isInRange = true;
+
+    if (args.length < ARGUMENT_WITH_NO_OPTION) {
+      printArgumentIsNotValid(NOT_ENOUGH_COMMAND_LINE_ARGUMENTS);
+      isInRange = false;
+    }
+    if (args.length > ARGUMENT_WITH_TWO_OPTION) {
+      printArgumentIsNotValid(TOO_MANY_COMMAND_LINE_ARGUMENTS);
+      isInRange = false;
     }
 
-    private static void printArgumentIsNotValid(String errorMessage) {
-        System.err.println(errorMessage);
-        System.err.println();
-        System.err.println(USAGE);
-        System.exit(1);
-    }
+    return isInRange;
+  }
 
+  private static boolean isNotOption(String args) {
+      return !(args == PRINT_OPTION || args == README_OPTION);
+  }
 
+  private static void printArgumentIsNotValid(String errorMessage) {
+      System.err.println(errorMessage);
+      System.err.println();
+      System.err.println(USAGE);
+      System.exit(1);
+  }
 
 }
--- airline/src/test/java/edu/pdx/cs410J/sokam/Project1Test.java
+++ airline/src/test/java/edu/pdx/cs410J/sokam/Project1Test.java
@@ -14,121 +14,111 @@ import edu.pdx.cs410J.InvokeMainTestCase;
  */
 public class Project1Test extends InvokeMainTestCase {
 
-    /**
-     * Invokes the main method of {@link Project1} with the given arguments.
-     */
-    private MainMethodResult invokeMain(String... args) {
-        return invokeMain( Project1.class, args );
-    }
+  /**
+   * Invokes the main method of {@link Project1} with the given arguments.
+   */
+  private MainMethodResult invokeMain(String... args) {
+    return invokeMain( Project1.class, args );
+  }
 
-     /**
-     * Tests that invoking the main method with no arguments issues an error
-     */
-    @Test
-    public void noArgumentHasExitCodeOf1() {
-        MainMethodResult result = invokeProjectMain();
-        assertThat(result.getExitCode(), equalTo(1));
-    }
+   /**
+   * Tests that invoking the main method with no arguments issues an error
+   */
+  @Test
+  public void noArgumentShouldError() {
+    MainMethodResult result = invokeProjectMain();
+    assertThat(result.getExitCode(), equalTo(1));
+  }
 
-    private MainMethodResult invokeProjectMain(String... args) {
-        return invokeMain(Project1.class, args);
-    }
+  private MainMethodResult invokeProjectMain(String... args) {
+    return invokeMain(Project1.class, args);
+  }
 
-    @Test
-    public void noArgumentPrintMissingArgumentToStandardError() {
-        String errorMessage = Project1.NOT_ENOUGH_COMMAND_LINE_ARGUMENTS;
-        assertThatStandardErrorContains(errorMessage);
-    }
+  @Test
+  public void noArgumentPrintMissingArgumentToStandardError() {
+    String errorMessage = Project1.NOT_ENOUGH_COMMAND_LINE_ARGUMENTS;
+    assertThatStandardErrorContains(errorMessage);
+  }
 
-    private void assertThatStandardErrorContains(String errorMessage, String... args) {
-        MainMethodResult result = invokeProjectMain(args);
-        assertThat(result.getErr(), containsString(errorMessage));
-        assertThat(result.getExitCode(), equalTo(1));
-    }
+  private void assertThatStandardErrorContains(String errorMessage, String... args) {
+    MainMethodResult result = invokeProjectMain(args);
+    assertThat(result.getErr(), containsString(errorMessage));
+    assertThat(result.getExitCode(), equalTo(1));
+  }
 
-    @Test
-    public void oneCommandLineArgumentPrintMissingArgumentToStandardError(){
-        String errorMessage = Project1.NOT_ENOUGH_COMMAND_LINE_ARGUMENTS;
-        assertThatStandardErrorContains(errorMessage, "fail");
-    }
+  @Test
+  public void oneCommandLineArgumentPrintMissingArgumentToStandardError(){
+    String errorMessage = Project1.NOT_ENOUGH_COMMAND_LINE_ARGUMENTS;
+    assertThatStandardErrorContains(errorMessage, "fail");
+  }
 
-    @Test
-    public void whenArgumentAreMissingUsageMessageIsPrintToStandardError(){
-        String errorMessage = Project1.USAGE;
-        assertThatStandardErrorContains(errorMessage);
-    }
+  @Test
+  public void missingArgumentShouldPrintUsageMessageToStandardError(){
+    String errorMessage = Project1.USAGE;
+    assertThatStandardErrorContains(errorMessage);
+  }
 
-    @Test
-    public void whenTooManyArgumentPrintArgumentOverflowToStandardError(){
-        String errorMessage = Project1.TOO_MANY_COMMAND_LINE_ARGUMENTS;
-        String[] args = createArgumentsForTest(Project1.ARGUMENT_WITH_TWO_OPTION + 1);
-        assertThatStandardErrorContains(errorMessage, args);
-    }
+  @Test
+  public void tooManyArgumentShouldPrintArgumentOverflowToStandardError(){
+    String errorMessage = Project1.TOO_MANY_COMMAND_LINE_ARGUMENTS;
+    String[] args = createArgumentsForTest(Project1.ARGUMENT_WITH_TWO_OPTION + 1);
+    assertThatStandardErrorContains(errorMessage, args);
+  }
 
-    private String[] createArgumentsForTest(int numberOfArguments) {
-        String[] args = new String[numberOfArguments];
-        for (int i = 0; i < numberOfArguments; i++)
-            args[i] = String.valueOf(i);
-        return args;
-    }
+  private String[] createArgumentsForTest(int numberOfArguments) {
+    String[] args = new String[numberOfArguments];
+    for (int i = 0; i < numberOfArguments; i++)
+      args[i] = String.valueOf(i);
+    return args;
+  }
 
-    @Test
-    public void whenThereAreLowerLimitOfArgumentExitCodeIsZero(){
-        String[] args = createArgumentsForTest(Project1.ARGUMENT_WITH_NO_OPTION);
-        assertThatArgumentsAreValid(args);
-    }
+  @Test
+  public void minNumberOfArgumentShouldExitCodeIsZero(){
+    String[] args = createArgumentsForTest(Project1.ARGUMENT_WITH_NO_OPTION);
+    assertThatArgumentsAreValid(args);
+  }
 
-    private void assertThatArgumentsAreValid(String... args) {
-        MainMethodResult result = invokeProjectMain(args);
-        assertThat(result.getExitCode(), equalTo(0));
-    }
+  private void assertThatArgumentsAreValid(String... args) {
+    MainMethodResult result = invokeProjectMain(args);
+    assertThat(result.getExitCode(), equalTo(0));
+  }
 
-//    @Test
-//    public void whenThereAreUpperLimitOfArgumentExitCodeIsZero(){
-//        String[] args = createArgumentsForTest(Project1.ARGUMENT_WITH_TWO_OPTION);
-//        assertThatArgumentsAreValid(args);
-//    }
+//  @Test
+//  public void whenThereAreUpperLimitOfArgumentExitCodeIsZero(){
+//    String[] args = createArgumentsForTest(Project1.ARGUMENT_WITH_TWO_OPTION);
+//    assertThatArgumentsAreValid(args);
+//  }
 
-    @Test
-    public void whenThereAreSevenArgumentFirstIsNotOptionExitCodeIs1(){
-        String[] args = createArgumentsForTest(Project1.ARGUMENT_WITH_NO_OPTION +1);
-        String errorMessage = Project1.INCORRECT_COMMAND_LINE_ARGUMENTS;
-        assertThatStandardErrorContains(errorMessage, args);
-    }
+  @Test
+  public void whenTwoOptionAllowButFirstTwoArgumentsAreNotOptionShouldError(){
+    String[] args = createArgumentsForTest(Project1.ARGUMENT_WITH_TWO_OPTION);
+    String errorMessage = Project1.INCORRECT_COMMAND_LINE_ARGUMENTS;
+    assertThatStandardErrorContains(errorMessage, args);
+  }
 
-    @Test
-    public void whenThereAreEightArgumentFirstTwoIsNotOptionExitCodeIs1(){
-        String[] args = createArgumentsForTest(Project1.ARGUMENT_WITH_TWO_OPTION);
-        String errorMessage = Project1.INCORRECT_COMMAND_LINE_ARGUMENTS;
-        assertThatStandardErrorContains(errorMessage, args);
-    }
+  @Test
+  public void whenOneOptionAllowButFirstArgumentIsNotOptionShouldError(){
+    String[] args = createArgumentsForTest(Project1.ARGUMENT_WITH_ONE_OPTION);
+    args[0] = Project1.PRINT_OPTION;
+    assertThatArgumentsAreValid(args);
+  }
 
-    @Test
-    public void whenThereAreSevenArgumentFirstIsOptionExitCodeIsZero(){
-        String[] args = createArgumentsForTest(Project1.ARGUMENT_WITH_ONE_OPTION);
-        args[0] = Project1.PRINT;
-        assertThatArgumentsAreValid(args);
-    }
+  @Test
+  public void whenTwoOptionAllowAndFirstTwoArgumentAreDifferentOptionExitCodeIsZero(){
+    String[] args = createArgumentsForTest(Project1.ARGUMENT_WITH_TWO_OPTION);
+    args[0] = Project1.PRINT_OPTION;
+    args[1] = Project1.README_OPTION;
+    assertThatArgumentsAreValid(args);
+  }
 
-    @Test
-    public void whenThereAreEightArgumentFirstTwoAreOptionExitCodeIsZero(){
-        String[] args = createArgumentsForTest(Project1.ARGUMENT_WITH_TWO_OPTION);
-        args[0] = Project1.PRINT;
-        args[1] = Project1.README;
-        assertThatArgumentsAreValid(args);
-    }
-
-    @Test
-    public void whenThereAreEightArgumentFirstTwoOptionAreSameExitCodeIs1(){
-        String[] args = createArgumentsForTest(Project1.ARGUMENT_WITH_TWO_OPTION);
-        args[0] = Project1.PRINT;
-        args[1] = Project1.PRINT;
-        String errorMessage = Project1.INCORRECT_COMMAND_LINE_ARGUMENTS;
-        assertThatStandardErrorContains(errorMessage, args);
-    }
-
-
-
+  @Test
+  public void whenTwoOptionAllowButFirstTwoArgumentAreSameShouldError(){
+    String[] args = createArgumentsForTest(Project1.ARGUMENT_WITH_TWO_OPTION);
+    args[0] = Project1.PRINT_OPTION;
+    args[1] = Project1.PRINT_OPTION;
+    String errorMessage = Project1.INCORRECT_COMMAND_LINE_ARGUMENTS;
+    assertThatStandardErrorContains(errorMessage, args);
+  }
 
 
 }
